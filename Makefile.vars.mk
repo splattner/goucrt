9f8afd493ca3759b626e72abaa147be96f040092
Sebastian Plattner <plattner@puzzle.ch>
2023-09-30 10:42:03 +0200

    ci: fix image name

--- Makefile.vars.mk
+++ Makefile.vars.mk
@@ -19,5 +19,5 @@ golangci_bin = $(go_bin)/golangci-lint
 
 
 # Image URL to use all building/pushing image targets
-GOUCRT_GHCR_IMG ?= splattner/goucrt:$(IMG_TAG)
+GOUCRT_GHCR_IMG ?= ghcr.io/splattner/goucrt:$(IMG_TAG)
 
